d5172243aa8be6ee36ce98575d11d3f4172a40d7
Youssef <elmatdor.yosef@gmail.com>
2023-11-06 23:12:04 +0200

    find jobs model

---
+++ lib/modules/jobs/domain/model/find_jobs_model.dart
@@ -0,0 +1,412 @@
+/// status : "Success"
+/// message : "get jobs"
+/// data : [{"id":15,"user_id":29,"username":"youssef","title":"test","description":"testing","budget":44,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Invite_only","talent_type":"independent","success_score":80,"english_level":"native_bilingual","languages":[{"id":6,"title":"Chinese","created_at":"2023-09-27T08:47:46.000000Z"}],"qualifications":[{"id":17,"qualification":"Åland","created_at":"2023-11-06T20:51:57.000000Z"}],"level_experince":"Intermediate","earned":"100","pay_type":"One time","expected_time":"form_3_to_6_months","number_payment":7,"payment_frequency":"Every two weeks","expected_hour_per_week":"more than 30 h","time_requirement_week":"more than 30 hrs/week","total_hours":"3 year","status":"open","image":[{"id":13,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1699303917_1000052643.jpg","created_at":"2023-11-06T20:51:57.000000Z"}],"created_at":"2023-11-06T20:51:57.000000Z"},{"id":14,"user_id":29,"username":"youssef","title":"test api","description":"test api","budget":10000,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Only_Bluefield_Talents","talent_type":"independent","success_score":80,"english_level":"basic","languages":[{"id":5,"title":"Japan","created_at":"2023-09-27T08:47:23.000000Z"}],"qualifications":[{"id":16,"qualification":"Africia","created_at":"2023-11-06T20:35:16.000000Z"}],"level_experince":"Expert","earned":"1k","pay_type":"Hourly","expected_time":"more_than_6_months","number_payment":4,"payment_frequency":"every_two_week","expected_hour_per_week":"more than 30 h","time_requirement_week":"2","total_hours":"2","status":"open","image":[{"id":12,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1699302916_F6lFxzgW8AAU3aZ.jpeg","created_at":"2023-11-06T20:35:16.000000Z"}],"created_at":"2023-11-06T20:35:16.000000Z"},{"id":11,"user_id":18,"username":"client","title":"Dolorem eum eaque qu","description":"Suscipit aspernatur","budget":2000,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Invite_only","talent_type":"independent","success_score":0,"english_level":"basic","languages":[{"id":6,"title":"Chinese","created_at":"2023-09-27T08:47:46.000000Z"}],"qualifications":[{"id":12,"qualification":"AL","created_at":"2023-10-21T14:30:37.000000Z"}],"level_experince":"Intermediate","earned":"0","pay_type":"One time","expected_time":"form_3_to_6_months","number_payment":2,"payment_frequency":"every_two_week","expected_hour_per_week":"more than 30 h","time_requirement_week":"7999","total_hours":"2000","status":"open","image":[{"id":9,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1697898637_client3.png","created_at":"2023-10-21T14:30:37.000000Z"}],"created_at":"2023-10-21T14:30:37.000000Z"},{"id":10,"user_id":19,"username":"test","title":"test api","description":"test api","budget":10000,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Only_Bluefield_Talents","talent_type":"independent","success_score":80,"english_level":"basic","languages":[{"id":5,"title":"Japan","created_at":"2023-09-27T08:47:23.000000Z"}],"qualifications":[{"id":11,"qualification":"Africia","created_at":"2023-10-09T17:48:47.000000Z"}],"level_experince":"Expert","earned":"1k","pay_type":"Hourly","expected_time":"more_than_6_months","number_payment":4,"payment_frequency":"every_week","expected_hour_per_week":"more than 30 h","time_requirement_week":"","total_hours":"","status":"open","image":[{"id":8,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1696873727_Vector (1).png","created_at":"2023-10-09T17:48:47.000000Z"}],"created_at":"2023-10-09T17:48:47.000000Z"},{"id":9,"user_id":19,"username":"test","title":"test api","description":"test api","budget":10000,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Only_Bluefield_Talents","talent_type":"independent","success_score":80,"english_level":"basic","languages":[{"id":5,"title":"Japan","created_at":"2023-09-27T08:47:23.000000Z"}],"qualifications":[{"id":10,"qualification":"Africia","created_at":"2023-10-09T17:48:17.000000Z"}],"level_experince":"Expert","earned":"1k","pay_type":"Hourly","expected_time":"more_than_6_months","number_payment":4,"payment_frequency":"every_week","expected_hour_per_week":"more than 30 h","time_requirement_week":"","total_hours":"","status":"open","image":[{"id":7,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1696873697_Vector (1).png","created_at":"2023-10-09T17:48:17.000000Z"}],"created_at":"2023-10-09T17:48:17.000000Z"},{"id":8,"user_id":19,"username":"test","title":"test api","description":"test api","budget":10000,"category_id":3,"category":"category1","project_type_id":3,"project_type":"One-time project","sub_category_id":4,"sub_category":"subCategory1","question_id":1,"question":"Do you have any questions about the job description?","cover_letter":1,"visibility":"Only_Bluefield_Talents","talent_type":"independent","success_score":80,"english_level":"basic","languages":[{"id":5,"title":"Japan","created_at":"2023-09-27T08:47:23.000000Z"}],"qualifications":[{"id":9,"qualification":"Africia","created_at":"2023-10-09T17:47:50.000000Z"}],"level_experince":"Expert","earned":"1k","pay_type":"Hourly","expected_time":"more_than_6_months","number_payment":4,"payment_frequency":"every_month","expected_hour_per_week":"more than 30 h","time_requirement_week":"","total_hours":"","status":"open","image":[{"id":6,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1696873670_Vector (1).png","created_at":"2023-10-09T17:47:50.000000Z"}],"created_at":"2023-10-09T17:47:50.000000Z"}]
+
+class FindJobsModel {
+  FindJobsModel({
+      this.status, 
+      this.message, 
+      this.data,});
+
+  FindJobsModel.fromJson(dynamic json) {
+    status = json['status'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = [];
+      json['data'].forEach((v) {
+        data?.add(FindJobsData.fromJson(v));
+      });
+    }
+  }
+  String? status;
+  String? message;
+  List<FindJobsData>? data;
+FindJobsModel copyWith({  String? status,
+  String? message,
+  List<FindJobsData>? data,
+}) => FindJobsModel(  status: status ?? this.status,
+  message: message ?? this.message,
+  data: data ?? this.data,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = status;
+    map['message'] = message;
+    if (data != null) {
+      map['data'] = data?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+
+/// id : 15
+/// user_id : 29
+/// username : "youssef"
+/// title : "test"
+/// description : "testing"
+/// budget : 44
+/// category_id : 3
+/// category : "category1"
+/// project_type_id : 3
+/// project_type : "One-time project"
+/// sub_category_id : 4
+/// sub_category : "subCategory1"
+/// question_id : 1
+/// question : "Do you have any questions about the job description?"
+/// cover_letter : 1
+/// visibility : "Invite_only"
+/// talent_type : "independent"
+/// success_score : 80
+/// english_level : "native_bilingual"
+/// languages : [{"id":6,"title":"Chinese","created_at":"2023-09-27T08:47:46.000000Z"}]
+/// qualifications : [{"id":17,"qualification":"Åland","created_at":"2023-11-06T20:51:57.000000Z"}]
+/// level_experince : "Intermediate"
+/// earned : "100"
+/// pay_type : "One time"
+/// expected_time : "form_3_to_6_months"
+/// number_payment : 7
+/// payment_frequency : "Every two weeks"
+/// expected_hour_per_week : "more than 30 h"
+/// time_requirement_week : "more than 30 hrs/week"
+/// total_hours : "3 year"
+/// status : "open"
+/// image : [{"id":13,"image":"http://bluefieldnet.techunits.net/public/storage/jobs/1699303917_1000052643.jpg","created_at":"2023-11-06T20:51:57.000000Z"}]
+/// created_at : "2023-11-06T20:51:57.000000Z"
+
+class FindJobsData {
+  FindJobsData({
+      this.id, 
+      this.userId, 
+      this.username, 
+      this.title, 
+      this.description, 
+      this.budget, 
+      this.categoryId, 
+      this.category, 
+      this.projectTypeId, 
+      this.projectType, 
+      this.subCategoryId, 
+      this.subCategory, 
+      this.questionId, 
+      this.question, 
+      this.coverLetter, 
+      this.visibility, 
+      this.talentType, 
+      this.successScore, 
+      this.englishLevel, 
+      this.languages, 
+      this.qualifications, 
+      this.levelExperince, 
+      this.earned, 
+      this.payType, 
+      this.expectedTime, 
+      this.numberPayment, 
+      this.paymentFrequency, 
+      this.expectedHourPerWeek, 
+      this.timeRequirementWeek, 
+      this.totalHours, 
+      this.status, 
+      this.image, 
+      this.createdAt,});
+
+  FindJobsData.fromJson(dynamic json) {
+    id = json['id'];
+    userId = json['user_id'];
+    username = json['username'];
+    title = json['title'];
+    description = json['description'];
+    budget = json['budget'];
+    categoryId = json['category_id'];
+    category = json['category'];
+    projectTypeId = json['project_type_id'];
+    projectType = json['project_type'];
+    subCategoryId = json['sub_category_id'];
+    subCategory = json['sub_category'];
+    questionId = json['question_id'];
+    question = json['question'];
+    coverLetter = json['cover_letter'];
+    visibility = json['visibility'];
+    talentType = json['talent_type'];
+    successScore = json['success_score'];
+    englishLevel = json['english_level'];
+    if (json['languages'] != null) {
+      languages = [];
+      json['languages'].forEach((v) {
+        languages?.add(Languages.fromJson(v));
+      });
+    }
+    if (json['qualifications'] != null) {
+      qualifications = [];
+      json['qualifications'].forEach((v) {
+        qualifications?.add(Qualifications.fromJson(v));
+      });
+    }
+    levelExperince = json['level_experince'];
+    earned = json['earned'];
+    payType = json['pay_type'];
+    expectedTime = json['expected_time'];
+    numberPayment = json['number_payment'];
+    paymentFrequency = json['payment_frequency'];
+    expectedHourPerWeek = json['expected_hour_per_week'];
+    timeRequirementWeek = json['time_requirement_week'];
+    totalHours = json['total_hours'];
+    status = json['status'];
+    if (json['image'] != null) {
+      image = [];
+      json['image'].forEach((v) {
+        image?.add(Image.fromJson(v));
+      });
+    }
+    createdAt = json['created_at'];
+  }
+  int? id;
+  int? userId;
+  String? username;
+  String? title;
+  String? description;
+  int? budget;
+  int? categoryId;
+  String? category;
+  int? projectTypeId;
+  String? projectType;
+  int? subCategoryId;
+  String? subCategory;
+  int? questionId;
+  String? question;
+  int? coverLetter;
+  String? visibility;
+  String? talentType;
+  int? successScore;
+  String? englishLevel;
+  List<Languages>? languages;
+  List<Qualifications>? qualifications;
+  String? levelExperince;
+  String? earned;
+  String? payType;
+  String? expectedTime;
+  int? numberPayment;
+  String? paymentFrequency;
+  String? expectedHourPerWeek;
+  String? timeRequirementWeek;
+  String? totalHours;
+  String? status;
+  List<Image>? image;
+  String? createdAt;
+FindJobsData copyWith({  int? id,
+  int? userId,
+  String? username,
+  String? title,
+  String? description,
+  int? budget,
+  int? categoryId,
+  String? category,
+  int? projectTypeId,
+  String? projectType,
+  int? subCategoryId,
+  String? subCategory,
+  int? questionId,
+  String? question,
+  int? coverLetter,
+  String? visibility,
+  String? talentType,
+  int? successScore,
+  String? englishLevel,
+  List<Languages>? languages,
+  List<Qualifications>? qualifications,
+  String? levelExperince,
+  String? earned,
+  String? payType,
+  String? expectedTime,
+  int? numberPayment,
+  String? paymentFrequency,
+  String? expectedHourPerWeek,
+  String? timeRequirementWeek,
+  String? totalHours,
+  String? status,
+  List<Image>? image,
+  String? createdAt,
+}) => FindJobsData(  id: id ?? this.id,
+  userId: userId ?? this.userId,
+  username: username ?? this.username,
+  title: title ?? this.title,
+  description: description ?? this.description,
+  budget: budget ?? this.budget,
+  categoryId: categoryId ?? this.categoryId,
+  category: category ?? this.category,
+  projectTypeId: projectTypeId ?? this.projectTypeId,
+  projectType: projectType ?? this.projectType,
+  subCategoryId: subCategoryId ?? this.subCategoryId,
+  subCategory: subCategory ?? this.subCategory,
+  questionId: questionId ?? this.questionId,
+  question: question ?? this.question,
+  coverLetter: coverLetter ?? this.coverLetter,
+  visibility: visibility ?? this.visibility,
+  talentType: talentType ?? this.talentType,
+  successScore: successScore ?? this.successScore,
+  englishLevel: englishLevel ?? this.englishLevel,
+  languages: languages ?? this.languages,
+  qualifications: qualifications ?? this.qualifications,
+  levelExperince: levelExperince ?? this.levelExperince,
+  earned: earned ?? this.earned,
+  payType: payType ?? this.payType,
+  expectedTime: expectedTime ?? this.expectedTime,
+  numberPayment: numberPayment ?? this.numberPayment,
+  paymentFrequency: paymentFrequency ?? this.paymentFrequency,
+  expectedHourPerWeek: expectedHourPerWeek ?? this.expectedHourPerWeek,
+  timeRequirementWeek: timeRequirementWeek ?? this.timeRequirementWeek,
+  totalHours: totalHours ?? this.totalHours,
+  status: status ?? this.status,
+  image: image ?? this.image,
+  createdAt: createdAt ?? this.createdAt,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['user_id'] = userId;
+    map['username'] = username;
+    map['title'] = title;
+    map['description'] = description;
+    map['budget'] = budget;
+    map['category_id'] = categoryId;
+    map['category'] = category;
+    map['project_type_id'] = projectTypeId;
+    map['project_type'] = projectType;
+    map['sub_category_id'] = subCategoryId;
+    map['sub_category'] = subCategory;
+    map['question_id'] = questionId;
+    map['question'] = question;
+    map['cover_letter'] = coverLetter;
+    map['visibility'] = visibility;
+    map['talent_type'] = talentType;
+    map['success_score'] = successScore;
+    map['english_level'] = englishLevel;
+    if (languages != null) {
+      map['languages'] = languages?.map((v) => v.toJson()).toList();
+    }
+    if (qualifications != null) {
+      map['qualifications'] = qualifications?.map((v) => v.toJson()).toList();
+    }
+    map['level_experince'] = levelExperince;
+    map['earned'] = earned;
+    map['pay_type'] = payType;
+    map['expected_time'] = expectedTime;
+    map['number_payment'] = numberPayment;
+    map['payment_frequency'] = paymentFrequency;
+    map['expected_hour_per_week'] = expectedHourPerWeek;
+    map['time_requirement_week'] = timeRequirementWeek;
+    map['total_hours'] = totalHours;
+    map['status'] = status;
+    if (image != null) {
+      map['image'] = image?.map((v) => v.toJson()).toList();
+    }
+    map['created_at'] = createdAt;
+    return map;
+  }
+
+}
+
+/// id : 13
+/// image : "http://bluefieldnet.techunits.net/public/storage/jobs/1699303917_1000052643.jpg"
+/// created_at : "2023-11-06T20:51:57.000000Z"
+
+class Image {
+  Image({
+      this.id, 
+      this.image, 
+      this.createdAt,});
+
+  Image.fromJson(dynamic json) {
+    id = json['id'];
+    image = json['image'];
+    createdAt = json['created_at'];
+  }
+  int? id;
+  String? image;
+  String? createdAt;
+Image copyWith({  int? id,
+  String? image,
+  String? createdAt,
+}) => Image(  id: id ?? this.id,
+  image: image ?? this.image,
+  createdAt: createdAt ?? this.createdAt,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['image'] = image;
+    map['created_at'] = createdAt;
+    return map;
+  }
+
+}
+
+/// id : 17
+/// qualification : "Åland"
+/// created_at : "2023-11-06T20:51:57.000000Z"
+
+class Qualifications {
+  Qualifications({
+      this.id, 
+      this.qualification, 
+      this.createdAt,});
+
+  Qualifications.fromJson(dynamic json) {
+    id = json['id'];
+    qualification = json['qualification'];
+    createdAt = json['created_at'];
+  }
+  int? id;
+  String? qualification;
+  String? createdAt;
+Qualifications copyWith({  int? id,
+  String? qualification,
+  String? createdAt,
+}) => Qualifications(  id: id ?? this.id,
+  qualification: qualification ?? this.qualification,
+  createdAt: createdAt ?? this.createdAt,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['qualification'] = qualification;
+    map['created_at'] = createdAt;
+    return map;
+  }
+
+}
+
+/// id : 6
+/// title : "Chinese"
+/// created_at : "2023-09-27T08:47:46.000000Z"
+
+class Languages {
+  Languages({
+      this.id, 
+      this.title, 
+      this.createdAt,});
+
+  Languages.fromJson(dynamic json) {
+    id = json['id'];
+    title = json['title'];
+    createdAt = json['created_at'];
+  }
+  int? id;
+  String? title;
+  String? createdAt;
+Languages copyWith({  int? id,
+  String? title,
+  String? createdAt,
+}) => Languages(  id: id ?? this.id,
+  title: title ?? this.title,
+  createdAt: createdAt ?? this.createdAt,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['title'] = title;
+    map['created_at'] = createdAt;
+    return map;
+  }
+
+}
